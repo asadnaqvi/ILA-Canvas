ad003b47681997b3be964ad412c569c4605af43b
CDOCS\asad.naqvi <removing initial commit>
2018-07-04 21:00:46 +0500

    adding material design
    adding material design

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,26 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {DashboardComponent} from './components/dashboard/dashboard.component';
+import { ObjectIdentificationComponent } from './components/object-identification/object-identification.component';
+import { FindMissingCharacterComponent } from './components/find-missing-character/find-missing-character.component';
+import { CountObjectsComponent } from './components/count-objects/count-objects.component';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path:'',
+  component:DashboardComponent
+},
+{
+  path:'oic',
+  component:ObjectIdentificationComponent
+},
+{
+  path:'fmcc',
+  component:FindMissingCharacterComponent
+},
+{
+  path:'coc',
+  component:CountObjectsComponent
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
