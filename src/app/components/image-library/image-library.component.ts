9b9549afa78bd0b278b1ee870dc0fe804c6ca688
CDOCS\asad.naqvi <asad.naqvi@wolterskluwer.com>
2018-07-18 15:01:59 +0500

    adding image library with optional params

--- src/app/components/image-library/image-library.component.ts
+++ src/app/components/image-library/image-library.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import{Router,ActivatedRoute} from '@angular/router';
 
 @Component({
   selector: 'app-image-library',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ImageLibraryComponent implements OnInit {
 LibraryImages = []
-  constructor() { }
+activityName:string=""
+  constructor(private route:ActivatedRoute) { }
 
   ngOnInit() {
+    this.route.paramMap.subscribe(stateParams=>{
+      this.activityName = stateParams.get("id")
+    });
     this.LibraryImages = [{
       title:"item 1",
       path:"assets/images/library/p1.jpeg",
@@ -27,10 +32,13 @@ LibraryImages = []
     },
     {
       title:"item 2",
-      path:"assets/images/library/p4.jpeg",
-      avatar:"assets/images/library/p4.jpeg"
+      path:"assets/images/library/coc/p4.jpeg",
+      avatar:"assets/images/library/coc/p4.jpeg"
     }
     ]
+    if(this.activityName){   
+    this.LibraryImages = this.LibraryImages.filter(item=>item.path.indexOf("/" + this.activityName + "/")>-1)
+    }    
   }
 
 }
